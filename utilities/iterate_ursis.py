8cf7c8239e44b09d3975604ecf71a6c6bfcab9da
Jon Clucas <jon.clucas@childmind.org>
2016-12-22 10:44:04 -0500

    reverse

--- utilities/iterate_ursis.py
+++ utilities/iterate_ursis.py
@@ -16,7 +16,7 @@ Created on Wed Dec 21 16:34:37 2016
 """
 import os
 
-def i_ursi(top_in, sub_in, function, parameters = None, conditions = None):
+def i_ursi(top_in, sub_in):
     """
     Function to iterate through [top_directory]/URSI/[subdirectory]/files
     structure and perform a function on each file.
@@ -29,59 +29,35 @@ def i_ursi(top_in, sub_in, function, parameters = None, conditions = None):
     sub_in : string
         the name of the subdirectory we want to read from
 
-    function : function
-        the function to perform on each file
-
-    parameters : list or None (optional)
-        list of parameters to pass the function. The string "ursi_i", if
-        included will be replaced with the ursi. The absolute path of the file
-        to be operated on will be inserted in the front of this list.
-
-    conditions : list or None (optional)
-        conditions to check before performing function.
-
     Returns
     -------
     result : list or None
-        returns either a list of all returns of the called function or None if
-        the function has no return.
+        returns either a list of all files to act upon.
     """
     # initialize result
     result = []
+    print("Getting files.")
     # get participant directories
     for root, dirs, files in os.walk(top_in):
         # get each participant
         for participant in dirs:
-            print(os.path.basename(participant))
-            # get participant subdirectories
-            for proot, pdirs, pfiles in os.walk(os.path.join(root,
-                                                participant)):
-                # get desired subdirectory
-                if pdirs == sub_in:
-                    # get files
-                    for pfile in pfiles:
-                        # if applicable, check for conditions
-                        try:
-                            for condition in conditions:
-                                condition = ''.join(pfile, condition)
-                                print(condition)
-                                if eval(condition):
-                                    # add filepath to parameters
-                                    try:
-                                        parameters = [pfile, *parameters]
-                                    except NameError:
-                                        parameters = [pfile]
-                                    # run the function
-                                    print(str(function(*parameters)))
-                                    result.append(function(*parameters))
-                        # otherwise, just run the function
-                        except NameError:
-                            # add filepath to parameters
-                            try:
-                                parameters = [pfile, *parameters]
-                            except NameError:
-                                parameters = [pfile]
-                            # run the function
-                            print(str(function(*parameters)))
-                            result.append(function(*parameters))
+            if (os.path.basename(participant).startswith("M") or
+                os.path.basename(participant).startswith("m")):
+                print(''.join(["From ", os.path.basename(participant)]))
+                # get participant subdirectories
+                for proot, pdirs, pfiles in os.walk(os.path.join(root,
+                                                    participant)):
+                    for pdir in pdirs:
+                        # get desired subdirectory
+                        if os.path.basename(pdir) == sub_in:
+                            # get files
+                            for qroot, qdirs, qfiles in os.walk(os.path.join(
+                                                                proot, pdir)):
+                                for qfile in qfiles:
+                                    if not os.path.basename(qfile).startswith(
+                                                            '.'):
+                                        rfile = os.path.join(qroot, qfile)
+                                        print(''.join(["    adding ", rfile]))
+                                        result.append(rfile)
+    print(''.join(["Returning ", str(len(result)), " files"]))
     return result
